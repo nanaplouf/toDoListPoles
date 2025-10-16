f10f1b7156ecd00ac82ba870be04b1a2405bab08
nanaplouf <vera78ds@gmail.com>
2025-10-16 14:56:00 +0200

    vérification des formulaire + message d'erreur

--- src/Controllers/TaskController.php
+++ src/Controllers/TaskController.php
@@ -8,6 +8,17 @@ class TaskController extends AbstractController
 {
     public function addTask()
     {
+        if (isset($_POST['addTask'])) {
+            $title = htmlspecialchars($_POST['title']);
+            $description = htmlspecialchars($_POST['description']);
+            $status = htmlspecialchars($_POST['status']);
+
+            $this->totalCheck('title', $title);
+            $this->totalCheck('description', $description);
+            $this->totalCheck('status', $status);
+
+            //$this->debug($this->arrayError);
+        }
         require_once(__DIR__ . "/../Views/formTask.view.php");
     }
 }
