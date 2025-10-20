6abe428dad548da9744a2dee46eb49eb07386602
nanaplouf <vera78ds@gmail.com>
2025-10-20 15:10:21 +0200

    fin du projet

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -2,26 +2,37 @@
 require_once(__DIR__ . "/partials/head.view.php");
 ?>
 <h1 class="text-center text-warning fw-bold m-2">Bienvenue</h1>
-<h2>Voilà les taches :</h2>
-
+<h2>Les tache Urgente</h2>
 <?php
-if (!empty($resultTasks)) {
-    foreach ($resultTasks as $task) {
+if (!empty($tasksUrgent)) {
+    foreach ($tasksUrgent as $task) {
 ?>
         <div class="card" style="width: 18rem;">
             <div class="card-body">
                 <h3 class="card-title"><?= $task->getTitle() ?></h3>
-                <span class="badge rounded-pill text-bg-warning"><?= $task->getStatus() ?></span>
+                <span class="badge rounded-pill text-bg-danger"><?= $task->getStatus() ?></span>
                 <p class="card-text"><?= $task->getDescription() ?></p>
                 <a href="/tache?id=<?= $task->getId() ?>" class="btn btn-primary">voir +</a>
+                <form action="" method="post">
+                    <input type="hidden" value="<?= $task->getId() ?>" name="id">
+                    <button type="submit" class="btn btn-success mt-2" name="editStatus">Terminer</button>
+                </form>
             </div>
         </div>
 <?php
     }
 } else {
-    echo "<p>Vous n'avez pas de tâche !</p>";
+    echo "<p>Vous n'avez pas de tâche urgente !</p>";
 }
 ?>
+<h2>Les tache A faire</h2>
+
+<h2>Les tache en cours</h2>
+
+<h2>Les tache terminé</h2>
+
+<h2>Voilà les taches :</h2>
+
 <?php
 require_once(__DIR__ . "/partials/footer.view.php");
 ?>
--- src/Views/showTask.view.php
+++ src/Views/showTask.view.php
@@ -8,6 +8,10 @@ require_once(__DIR__ . "/partials/head.view.php");
 <p><?= $myTask->getCreationDate() ?></p>
 <p><?= $myTask->getModificationDate() ?></p>
 <a href="/modifier?id=<?= $myTask->getId() ?>" class="btn btn-warning">Modifier</a>
+<form action="/supprimer?id=<?= $myTask->getId() ?>" method="post">
+    <input type="hidden" value="<?= $myTask->getId() ?>" name="id">
+    <button class="btn btn-danger mt-2" name="delete" type="submit">Supprimer</button>
+</form>
 <?php
 require_once(__DIR__ . "/partials/footer.view.php");
 ?>
